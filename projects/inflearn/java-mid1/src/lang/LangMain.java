a2229bd6374677ba2e8ccc159abd0917e36682e6
sklee2685 <sklee2685@gmail.com>
2024-06-07 15:16:10 +0900

    java day23

---
+++ projects/inflearn/java-mid1/src/lang/LangMain.java
@@ -0,0 +1,9 @@
+package lang;
+
+//import java.lang.System;
+//본래 java.lang 패키지를 사용하기 위해 import을 해야 하지만 생략이 가능하고, 기본적으로 import되어 있음
+public class LangMain { //java.lang 패키지
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
