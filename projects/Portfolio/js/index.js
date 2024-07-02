0efda656a7b9e4b7ce6abf33c301333aa56bc033
sklee2685 <sklee2685@gmail.com>
2024-07-02 16:16:59 +0900

    +누르면 목록 추가

--- projects/Portfolio/js/index.js
+++ projects/Portfolio/js/index.js
@@ -7,11 +7,40 @@ function toggleActiveClass() {
     container.classList.toggle('active');
 }
 
-// 각 토글 버튼에 공통 함수 할당
+//각 토글 버튼에 공통 함수 할당
 toggleButtons.forEach(function(button) {
     button.onclick = toggleActiveClass;
 });
 
 logout.onclick=function(){
     location.replace("login.html");
+}
+
+function addItem() {
+
+    if (confirm("메뉴에 목록을 추가하겠습니까?") == true){
+        const list = document.getElementById('list');
+
+        // li 요소 생성
+        const newItem = document.createElement('li');
+        newItem.className = 'item';
+        
+        // i 요소 생성 및 추가
+        const icon = document.createElement('i');
+        icon.className = 'icon';
+        newItem.appendChild(icon);
+        
+        // span 요소 생성 및 추가
+        const text = document.createElement('span');
+        var name = prompt("추가할 목록의 이름:");
+        text.textContent = name;
+        newItem.appendChild(text);
+        
+        // ul에 새로운 li 요소 추가
+        list.appendChild(newItem);
+
+    }else{
+        return false;
+    }
+    
 }
