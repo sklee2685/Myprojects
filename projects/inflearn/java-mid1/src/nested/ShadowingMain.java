4d404933d50039d5235397b8894440ff46b805a5
sklee2685 <sklee2685@gmail.com>
2024-07-30 13:17:38 +0900

    java 폴더 수정

---
+++ projects/inflearn/java-mid1/src/nested/ShadowingMain.java
@@ -0,0 +1,23 @@
+package nested.nested;
+
+public class ShadowingMain {//같은 이름의 바깥 변수 접근
+
+    public int value = 1;
+
+    class Inner {
+        public int value = 2;
+
+        void go(){
+            int value = 3;
+            System.out.println("value = " + value);
+            System.out.println("this.value = " + this.value);
+            System.out.println("ShadowingMain.value = " + ShadowingMain.this.value);
+        }
+    }
+
+    public static void main(String[] args) {
+        ShadowingMain shadowingMain = new ShadowingMain();
+        Inner inner = shadowingMain.new Inner();
+        inner.go();
+    }
+}
