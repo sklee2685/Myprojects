4a0780475014796ff21f6f610770b7bd2d18da09
sklee2685 <sklee2685@gmail.com>
2024-09-07 21:27:43 +0900

    springboot

---
+++ projects/internet lectures/SptringBoot_test/SptringBoot_test/src/main/java/com/example/SptringBoot_test/SptringBootTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.SptringBoot_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptringBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptringBootTestApplication.class, args);
+	}
+
+}
---
+++ projects/internet lectures/SptringBoot_test/SptringBoot_test/src/test/java/com/example/SptringBoot_test/SptringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SptringBoot_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
