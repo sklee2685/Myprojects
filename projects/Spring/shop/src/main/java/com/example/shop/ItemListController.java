64b5c18765ddff11bacf5549f63ffef6b696e8d0
sklee2685 <sklee2685@gmail.com>
2024-09-23 22:42:20 +0900

    상세페이지

--- projects/Spring/shop/src/main/java/com/example/shop/ItemListController.java
+++ projects/Spring/shop/src/main/java/com/example/shop/ItemListController.java
@@ -3,12 +3,10 @@ package com.example.shop;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor //Lombok 문법
@@ -31,7 +29,7 @@ public class ItemListController { //상품목록 페이지
         System.out.println(date.get(0).getTitle()); // 1번째 행의 title
         System.out.println(date.get(0).getPrice()); // 1번째 행의 price
 
-        model.addAttribute("items",itemRepository.findAll());
+        model.addAttribute("items", itemRepository.findAll());
         return "list.html";
     }
 
@@ -49,9 +47,23 @@ public class ItemListController { //상품목록 페이지
     //    return "redirect:/list";
     //}
 
+    //서버에서 db로 데이터 전송 (위에 코드는 아래 코드를 자세히)
     @PostMapping("/itemAdd")
     String addPost(@ModelAttribute Item items) {
         itemRepository.save(items);
         return "redirect:/list";
+    }
+
+    //상품 상세페이지 만들기
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Long id,Model model) {
+        Optional<Item> byId = itemRepository.findById(id);
+        if (byId.isPresent()) {//전달 받은 id값을 기반으로 데이터가 있는지 검사
+            model.addAttribute("id", itemRepository.findById(id).orElse(null));
+            return "detail.html";
+        }else{
+            System.out.println("오류 찾는 데이터가 없습니다.");
+            return "redirect:/list";
+        }
     }
 }
